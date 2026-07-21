acf83319094578380f8e0676c7fb77568f655f36
YAAZHINI S <yaazhssk7@gmail.com>
2026-07-21 14:26:24 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> ss;
+        for(auto i:s) ss[i]++;
+        for(auto i: t){
+            ss[i]--;
+            // what if t has extra chars
+            if(ss[i]<0) return false;
+        }
+        for(auto i: ss){
+            // what if s has extra chars
+            if(i.second!=0) return false;
+        }
+        return true;
+    }
+};
